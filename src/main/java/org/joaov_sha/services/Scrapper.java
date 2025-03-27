e27550f0da6f87380e0cbf75cb903b8b628dfe7b
joaov-sha <joaovitor8832@gmail.com>
2025-03-27 14:21:37 -0300

    Adicionado o código para criação de diretório caso não haja, download de arquivos e compactação.

--- src/main/java/org/joaov_sha/services/Scrapper.java
+++ src/main/java/org/joaov_sha/services/Scrapper.java
@@ -1,11 +1,16 @@
 package org.joaov_sha.services;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import javax.swing.JOptionPane;
@@ -15,7 +20,7 @@ public class Scrapper {
     public Scrapper(){}
 
     public static void scrap(List<String> urlArquivos, String caminhoPasta, String caminhoZip){
-        
+
         try {
             Files.createDirectories(Paths.get(caminhoPasta));
             baixarEZiparArquivos(urlArquivos, caminhoZip);
@@ -30,7 +35,34 @@ public class Scrapper {
             for (String url : urlArquivos) {
                 String nomeDoArquivo = url.substring(url.lastIndexOf("/") + 1);
                 Path arquivoTemporario = Files.createTempFile("download_", "_" + nomeDoArquivo);
+                
+                baixarArquivo(url, arquivoTemporario.toString());
+
+                adicionarArquivosParaZip(arquivoTemporario, nomeDoArquivo, zipar);
+
+                Files.delete(arquivoTemporario);
             }
+        }
+    }
+
+    public static void baixarArquivo(String urlArquivo, String caminhoPasta) throws IOException{
+        URL url = new URL(urlArquivo);
+        try(InputStream entrada = url.openStream()){
+            Files.copy(entrada, Path.of(caminhoPasta), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    public static void adicionarArquivosParaZip(Path arquivoTemporario, String nomeDoArquivo, ZipOutputStream zipar) throws IOException{
+        try(FileInputStream fis = new FileInputStream(arquivoTemporario.toFile())){
+            zipar.putNextEntry(new ZipEntry(nomeDoArquivo));
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while((length = fis.read(buffer)) >= 0){
+                zipar.write(buffer, 0, length);
+            }
+
+            zipar.closeEntry();
         }
     }
 }
